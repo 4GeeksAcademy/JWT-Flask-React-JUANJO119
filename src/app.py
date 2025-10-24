f3cc2c56e18b7c1915b3c9f0d520fc393c65b073
Juanjo Villarejo Cespedes <jota_villarejo22@hotmail.com>
2025-10-24 18:55:56 +0000

    archivo app end point login/register

--- src/app.py
+++ src/app.py
@@ -5,8 +5,9 @@ import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
@@ -18,6 +19,9 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+app.config["JW_KEY"] = os.getenv('JW_KEY')
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -64,6 +68,46 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
+
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg': 'Envia información al body'}), 400
+    if 'email' not in body:
+        return jsonify({'msg': 'Campo email obligatorio'}), 400
+    if 'password' not in body:
+        return jsonify({'msg': 'Campo password obligatorio'}), 400
+
+    user_registered = User.query.filter_by(
+        email=body['email'], password=body['password']).first()
+    if user_registered is None:
+        return jsonify({'msg': 'Usuario o Contraseña incorrecta'}), 400
+    
+    access_token = create_access_token(identity=user_registered.email)
+
+    return jsonify({'msg': 'Todo salió bien', 'token': access_token}), 200
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg':'Envia información al body'}), 400
+    if 'email' not in body:
+        return jsonify({'msg': 'Campo email obligatorio'}), 400
+    if 'password' not in body:
+        return jsonify({'msg': 'Campo password obligatorio'}), 400
+    user = User()
+    user.email = body['email']
+    user.password = body['password']
+    user.is_active = True
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'msg': 'Usuario registrado con éxito'}), 200
+
 
 
 # this only runs if `$ python src/main.py` is executed
